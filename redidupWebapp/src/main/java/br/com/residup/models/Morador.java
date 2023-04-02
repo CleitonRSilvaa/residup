410ef1efde573438d520c95e290c602387d0384c
Cleiton.Silva <cleiton_ribeiro@live.com>
2023-04-02 16:01:28 -0300

    feat(residupWebapp): Adicionando tela de cadastro de moradores com incert no banco (sem tratamento erro)

--- redidupWebapp/src/main/java/br/com/residup/models/Morador.java
+++ redidupWebapp/src/main/java/br/com/residup/models/Morador.java
@@ -1,4 +1,122 @@
 package br.com.residup.models;
 
 public class Morador {
+    private int id ;
+    private String nome;
+    private String  sobrenome;
+    private String  cpf;
+    private String  rg;
+    private String  telefone;
+    private String email;
+    private String  numeroApartamento;
+    private String  bloco;
+    private String  senhaDeAcesso;
+
+    public Morador(String nome, String sobrenome, String cpf, String rg, String numeroApartamento, String bloco, String senhaDeAcesso) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.numeroApartamento = numeroApartamento;
+        this.bloco = bloco;
+        this.senhaDeAcesso = senhaDeAcesso;
+    }
+
+    public static String[] separarNomeSobrenome(String nomeCompleto) {
+        String[] partesNome = nomeCompleto.trim().split("\\s+");
+
+        if (partesNome.length < 2) {
+            throw new IllegalArgumentException("Nome incompleto, deve ter pelo menos nome e sobrenome.");
+        }
+
+        String nome = partesNome[0];
+        String sobrenome = partesNome[partesNome.length - 1];
+
+        for (int i = 1; i < partesNome.length - 1; i++) {
+            nome += " " + partesNome[i];
+        }
+
+        return new String[]{nome, sobrenome};
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getNumeroApartamento() {
+        return numeroApartamento;
+    }
+
+    public void setNumeroApartamento(String numeroApartamento) {
+        this.numeroApartamento = numeroApartamento;
+    }
+
+    public String getBloco() {
+        return bloco;
+    }
+
+    public void setBloco(String bloco) {
+        this.bloco = bloco;
+    }
+
+    public String getSenhaDeAcesso() {
+        return senhaDeAcesso;
+    }
+
+    public void setSenhaDeAcesso(String senhaDeAcesso) {
+        this.senhaDeAcesso = senhaDeAcesso;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- redidupWebapp/src/main/java/br/com/residup/servlets/CadastroMorador.java
+++ redidupWebapp/src/main/java/br/com/residup/servlets/CadastroMorador.java
@@ -1,4 +1,43 @@
 package br.com.residup.servlets;
 
-public class CadastroMorador {
+import br.com.residup.daos.MoradorDao;
+import br.com.residup.models.Morador;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/create_morador")
+public class CadastroMorador extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        var nomeCompleto = request.getParameter("nome");
+        var cpf = request.getParameter("cpf");
+        var rg = request.getParameter("rg");
+        var bloco = request.getParameter("bloco");
+        var email = request.getParameter("email");
+        var telefone = request.getParameter("telefone");
+        var numero_apartamento = request.getParameter("numeroApartamento");
+        var senha = request.getParameter("senha");
+
+        try {
+            String[] nomeSobrenome = Morador.separarNomeSobrenome(nomeCompleto);
+            String nome = nomeSobrenome[0];
+            String sobrenome = nomeSobrenome[1];
+            var morador = new Morador(nome,sobrenome,cpf,rg,numero_apartamento,bloco,senha);
+            if(MoradorDao.createMorador(morador)){
+                request.getRequestDispatcher("CadastrarMorador.html").forward(request, response);
+            }
+        }catch (Exception e){
+            System.out.println(e);
+            request.getRequestDispatcher("CadastrarMorador.html").forward((ServletRequest) request, (ServletResponse) e);
+        }
+
+    }
+
 }
