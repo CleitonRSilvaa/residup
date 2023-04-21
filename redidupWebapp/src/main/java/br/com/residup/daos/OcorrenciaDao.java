9e20e2d000f2cf1d0607ad8e861dc1df41731a82
Ana Carolina Guimarães <a.carolina1903@gmail.com>
2023-04-20 22:41:11 -0300

    feat(ocorrencia): criando logica de registrar ocorrencia

--- redidupWebapp/src/main/java/br/com/residup/daos/OcorrenciaDao.java
+++ redidupWebapp/src/main/java/br/com/residup/daos/OcorrenciaDao.java
@@ -4,7 +4,9 @@ import br.com.residup.models.Ocorrencia;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -59,24 +61,26 @@ public class OcorrenciaDao {
         return retorno;
     }
 
-    public static boolean listar(Ocorrencia ocorrencia) {
-        boolean retorno = false;
+    public static ArrayList<Ocorrencia> listar(Ocorrencia ocorrencia) {
         try (Connection connection = abrirConexao();
              PreparedStatement instrucao = connection
                      .prepareStatement("SELECT * FROM REGISTRO_OCORRENCIA")) {
-            int linhasRetorno = instrucao.executeUpdate();
-
-            if (linhasRetorno > 0) {
-                retorno = true;
-            } else {
-                retorno = false;
+            ArrayList<Ocorrencia> ocorrencias = new ArrayList<>();
+            ResultSet rs = instrucao.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String titulo = rs.getString(2);
+                String texto = rs.getString(3);
+                String resolucao = rs.getString(4);
+                var registroOcorrencia = new Ocorrencia(titulo, texto, resolucao);
+                registroOcorrencia.setId(id);
+                ocorrencias.add(registroOcorrencia);
             }
-
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            return ocorrencias;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
         }
-
-        return retorno;
     }
 
 
@@ -84,7 +88,7 @@ public class OcorrenciaDao {
         boolean retorno = false;
         try (Connection connection = abrirConexao();
              PreparedStatement instrucao = connection
-                     .prepareStatement("SELECT * FROM MORADOR WHERE CPF = ? AND SENHA_ACESSO = ?")) {
+                     .prepareStatement(" * FROM MORADOR WHERE CPF = ? AND SENHA_ACESSO = ?")) {
             instrucao.setString(1, morador.getCpf());
             instrucao.setString(2, morador.getSenhaDeAcesso());
 
--- redidupWebapp/src/main/java/br/com/residup/models/Ocorrencia.java
+++ redidupWebapp/src/main/java/br/com/residup/models/Ocorrencia.java
@@ -1,8 +1,25 @@
 package br.com.residup.models;
 
 public class Ocorrencia {
+    private int id;
     private String titulo;
     private String texto;
+    private String resolucao;
+
+    public String getResolucao() {
+        return resolucao;
+    }
+
+    public void setResolucao(String resolucao) {
+        this.resolucao = resolucao;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getTitulo() {
         return titulo;
@@ -20,8 +37,9 @@ public class Ocorrencia {
         this.texto = texto;
     }
 
-    public Ocorrencia(String titulo, String texto) {
+    public Ocorrencia(String titulo, String texto, String resolucao) {
         this.titulo = titulo;
         this.texto = texto;
+        this.resolucao = resolucao;
     }
 }
