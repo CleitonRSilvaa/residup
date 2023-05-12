2c6589f0bfa8abadda1807c87dec6344e05a8ccc
Cleiton.Silva <cleiton_ribeiro@live.com>
2023-05-11 22:31:22 -0300

    feat(Reserva Area): criando DAO da reserva de areas

--- residupWebapp/src/main/java/br/com/residup/daos/ReservaDao.java
+++ residupWebapp/src/main/java/br/com/residup/daos/ReservaDao.java
@@ -1,4 +1,140 @@
 package br.com.residup.daos;
 
+
+
+import br.com.residup.models.Reserva;
+import br.com.residup.models.Visitante;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static br.com.residup.shared.GerenciadorConexaoH2.abrirConexao;
+
 public class ReservaDao {
+
+    private static ReservaDao instance;
+    private Connection connection;
+
+    private ReservaDao() {
+        this.handleOpenConnection();
+    }
+
+    private void handleOpenConnection() {
+        try {
+            this.connection = abrirConexao();
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LoginDao.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static ReservaDao getInstance() {
+        if (instance == null) {
+            instance = new ReservaDao();
+        }
+        return instance;
+    }
+
+    public boolean moradorTemRederva(int id_morador) throws SQLException {
+        boolean retorno = false;
+        String qurey = "SELECT ID_MORADOR FROM RESERVA_AREA WHERE ID_MORADOR  = ?;";
+        try {
+            PreparedStatement pst = connection.prepareStatement(qurey);
+            pst.setInt(1, id_morador);
+            ResultSet rs = pst.executeQuery();
+            retorno = rs.next();
+
+            connection.close();
+            return retorno;
+        }catch (Exception exception){
+            System.out.println(exception.getLocalizedMessage());
+            return retorno;
+        }
+
+    }
+
+
+    public boolean TemReserva(Reserva reserva) throws SQLException {
+        boolean retorno = false;
+        String qurey = "SELECT * FROM RESERVA_AREA WHERE  " +
+                "DATA_RESERVA = ?  " +
+                "AND HORA_RESERVA  = ?  " +
+                "AND ID_AREA = ?;";
+        try {
+            PreparedStatement pst = connection.prepareStatement(qurey);
+            pst.setString(1, reserva.getDate_reserva());
+            pst.setString(2, reserva.getHora_reserva());
+            pst.setInt(3, reserva.getId_morador());
+            ResultSet rs = pst.executeQuery();
+            retorno = rs.next();
+
+            connection.close();
+            return retorno;
+        }catch (SQLException sqlException){
+            System.out.println(sqlException.getLocalizedMessage());
+            throw new RuntimeException("----->>>> Erro ao executar query TemReserva() ");
+
+        }catch (Exception exception){
+            System.out.println(exception.getLocalizedMessage());
+            return retorno;
+        }
+
+    }
+
+    public boolean insertReserva(Reserva reserva) {
+        Boolean retorno = false;
+        String create = "INSERT INTO RESERVA_AREA (DATA_RESERVA, HORA_RESERVA, ID_MORADOR, ID_AREA)\n" +
+                "VALUES (TIMESTAMP ?, ?, ?, ?);";
+        try {
+//            Connection connection = abrirConexao();
+            PreparedStatement pst = connection.prepareStatement(create);
+            pst.setString(1, reserva.getDate_reserva());
+            pst.setString(2, reserva.getHora_reserva());
+            pst.setInt(3, reserva.getId_morador());
+            pst.setInt(4, reserva.getId_area());
+
+            int linhasRetorno =  pst.executeUpdate();
+            connection.close();
+            return linhasRetorno > 0;
+
+        } catch (SQLException sqlException) {
+            System.out.println(sqlException.getLocalizedMessage());
+            throw new RuntimeException("----->>>> Erro ao executar insert reserva ");
+        }catch (Exception e){
+            System.out.println(e.getLocalizedMessage());
+            return retorno;
+        }
+    }
+    public boolean deleteReserva(Reserva reserva) {
+        Boolean retorno = false;
+        String create = "DELETE * FROM RESERVA_AREA WHERE ID_RESERVA = ? AND ID_MORADOR = ?);";
+        try {
+//            Connection connection = abrirConexao();
+            PreparedStatement pst = connection.prepareStatement(create);
+            pst.setString(1, reserva.getDate_reserva());
+            pst.setString(2, reserva.getHora_reserva());
+            pst.setInt(3, reserva.getId_morador());
+            pst.setInt(4, reserva.getId_area());
+
+            int linhasRetorno =  pst.executeUpdate();
+            connection.close();
+            return linhasRetorno > 0;
+
+        } catch (SQLException sqlException) {
+            System.out.println(sqlException.getLocalizedMessage());
+            throw new RuntimeException("----->>>> Erro ao executar insert reserva ");
+        }catch (Exception e){
+            System.out.println(e.getLocalizedMessage());
+            return retorno;
+        }
+    }
+
+
+
 }
--- residupWebapp/src/main/java/br/com/residup/models/Reserva.java
+++ residupWebapp/src/main/java/br/com/residup/models/Reserva.java
@@ -9,11 +9,55 @@ public class Reserva {
     int id_morador;
     int id_area;
 
-    Date date_reserva;
+    String date_reserva;
     String area;
     String hora_reserva;
 
+    public int getId_reserva() {
+        return id_reserva;
+    }
 
+    public void setId_reserva(int id_reserva) {
+        this.id_reserva = id_reserva;
+    }
 
+    public int getId_morador() {
+        return id_morador;
+    }
 
+    public void setId_morador(int id_morador) {
+        this.id_morador = id_morador;
+    }
+
+    public int getId_area() {
+        return id_area;
+    }
+
+    public void setId_area(int id_area) {
+        this.id_area = id_area;
+    }
+
+    public String getDate_reserva() {
+        return date_reserva;
+    }
+
+    public void setDate_reserva(String date_reserva) {
+        this.date_reserva = date_reserva;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getHora_reserva() {
+        return hora_reserva;
+    }
+
+    public void setHora_reserva(String hora_reserva) {
+        this.hora_reserva = hora_reserva;
+    }
 }
