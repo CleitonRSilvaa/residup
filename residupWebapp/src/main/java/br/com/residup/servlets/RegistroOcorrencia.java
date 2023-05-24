29bedd03d694beefcb4cd472c512bec9c66d846f
Ana Carolina Guimarães <a.carolina1903@gmail.com>
2023-05-24 09:10:55 -0300

    fix(Ocorrencia): corrigindo possivel erro 500 de cast

--- residupWebapp/src/main/java/br/com/residup/servlets/RegistroOcorrencia.java
+++ residupWebapp/src/main/java/br/com/residup/servlets/RegistroOcorrencia.java
@@ -78,7 +78,7 @@ public class RegistroOcorrencia extends HttpServlet {
         String mgs = (String) request.getSession().getAttribute("mgsJS");
 
         if (parametro != null && parametro) {
-            String msg = mgs;
+            String msg = mgs != null ? mgs : "";
             request.setAttribute("mensagem", msg);
         }
 
@@ -90,6 +90,7 @@ public class RegistroOcorrencia extends HttpServlet {
         response.setStatus(HttpServletResponse.SC_OK);
         rd.forward(request, response);
     }
+
 
 
     protected void registrarOcorrencia(HttpServletRequest request, HttpServletResponse response)
